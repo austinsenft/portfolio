2cee2d563d15a912f95fddf80357695a0680fd67
Austin Senft <austinsenft@gmail.com>
2024-06-16 08:16:54 -0600

    adding my image and hero section work

--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -1,3 +1,28 @@
 export default function HeroSection() {
-    return <h1>Hero Section</h1>;
+    return (
+        <section id="heroSection" className="hero--section">
+            <div className="hero--section--content--box">
+                <div className="hero--section-content">
+                    <p className="section--title">Hey, I'm Austin</p>
+                    <h1 className="hero--section--title">
+                        <span className="hero--section-title--color"> Full Stack</span>{" "}
+                        <br /> 
+                        Developer
+                    </h1>
+                    <p className="hero--section-description">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                    <br /> Nulla repellat, accusamus laboriosam enim explicabo velit voluptate commodi cupiditate
+                    </p>
+                    
+                </div>
+                <button className="btn btn-primary">
+                    Get In Touch
+                </button>
+
+            </div>
+            <div className="hero--section--img">
+                <img src="./img/hero_img.png" alt="Hero Section" style={{height:500, width:550}} />
+            </div>
+        </section>
+    )
 }
