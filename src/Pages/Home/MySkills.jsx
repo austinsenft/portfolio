309a22d145805a7f3e06ae20f4001012acbfeb60
Austin Senft <austinsenft@gmail.com>
2024-06-18 09:53:04 -0600

    finishing up my skills section. still need to go back and add details

--- src/Pages/Home/MySkills.jsx
+++ src/Pages/Home/MySkills.jsx
@@ -10,14 +10,17 @@ export default function MySkills() {
                 <p className="section--title">My Skills</p>
                 <h2 className="skills--section--heading">My Expertise</h2>
             </div>
-            <div className="skills--section--containter">
+            <div className="skills--section--container">
                 {/* Mapping skills with images referenced in index.json */}
                 {data?.skills?.map((item, index) => (
                     <div key={index} className="skills--section--card">
                         <div className="skills--section--img">
                             <img src={item.src} alt="Product Chain" />
                         </div>
-
+                        <div className="skills--section--card--content">
+                            <h3 className="skills--section--title">{item.title}</h3>
+                            <p className="skills--section--description">{item.description}</p>
+                        </div>
                     </div>
                 ))}
             </div>
