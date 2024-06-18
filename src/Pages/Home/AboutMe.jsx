3c5c40af730bbd8c6e17821e4be6afd4d50f1887
Austin Senft <austinsenft@gmail.com>
2024-06-18 11:14:52 -0600

    about me section setup and starting my portfolio

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,21 @@
+export default function AboutMe() {
+    return (
+        <section id="AboutMe" className="about--section">
+            <div className="about--section--img">
+                <img src="./img/about-me.jpeg" alt="About Me"></img>
+            </div>
+            <div className="hero--section--content--box about --section--box">
+            <div className="hero--section--content">
+            <p className="section--title">About</p>
+            <h1 className="skills--section--heading">About Me</h1>
+            <p className="hero--section--description">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque, eos in minima quos earum accusamus deserunt laboriosam mollitia iure quis voluptas velit sapiente necessitatibus officiis non! Molestiae voluptate harum commodi!
+            </p>
+            <p className="hero--section--description">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque, eos in minima quos earum accusamus deserunt laboriosam mollitia iure quis voluptas.
+            </p>
+            </div>
+            </div>
+        </section>
+    )
+}
