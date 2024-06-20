44c62de62d03a370681be7f323b9709434ee6b72
Austin Senft <austinsenft@gmail.com>
2024-06-19 20:25:48 -0600

    got border on hover for pics of me

--- src/Pages/Home/ContactMe.jsx
+++ src/Pages/Home/ContactMe.jsx
@@ -3,6 +3,10 @@ export default function ContactMe() {
         <section id="Contact" className="contact--section">
             <div>
                 <p className="sub--title"> Get In Touch</p>
+                <h2>Contact Me</h2>
+                <p className="text-lg">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum tenetur
+                </p>
             </div>
         </section>
     )
