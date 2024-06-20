6374a758bf056cb519cd8ae768da2b4f99c6e123
Austin Senft <austinsenft@gmail.com>
2024-06-19 20:09:12 -0600

    fomratting

---
+++ src/Pages/Home/ContactMe.jsx
@@ -0,0 +1,9 @@
+export default function ContactMe() {
+    return (
+        <section id="Contact" className="contact--section">
+            <div>
+                <p className="sub--title"> Get In Touch</p>
+            </div>
+        </section>
+    )
+}
--- src/Pages/Home/Navbar.jsx
+++ src/Pages/Home/Navbar.jsx
@@ -2,35 +2,36 @@ import { useState, useEffect } from "react";
 import { Link } from "react-scroll"
 
 function Navbar() {
-    const [navActive, setNavActive] = useState(false);
+  const [navActive, setNavActive] = useState(false);
 
-    const toggleNav = () => {
-        setNavActive(!navActive)
+  const toggleNav = () => {
+    setNavActive(!navActive);
+  };
+
+  const closeMenu = () => {
+    setNavActive(false);
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 500) {
+        closeMenu;
+      }
     };
 
-    const closeMenu = () => {
-        setNavActive(false)
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
     };
+  }, []);
 
-    useEffect(() => {
-        const handleResize = () => {
-            if (window.innerWidth <= 500) {
-                closeMenu
-            }
-        };
+  useEffect(() => {
+    if (window.innerWidth <= 1200) {
+      closeMenu;
+    }
+  }, []);
 
-        window.addEventListener("resize", handleResize)
-
-        return () => {
-            window.removeEventListener("resize", handleResize)
-        };
-    }, []);
-
-    useEffect(() => {
-        if (window.innerWidth <= 1200) {
-            closeMenu;
-        }
-    }, []);
 
     return (
         <nav className={`navbar ${navActive ? "active" : ""}`}>
@@ -130,7 +131,7 @@ function Navbar() {
                 Contact Me
             </Link>
         </nav>
-    )
+    );
 }
 
 export default Navbar; 
